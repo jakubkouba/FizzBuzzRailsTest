1f21790f23586b47019cab1ed6e49bdb361543f3
Jakub Adler <adler.jakub@gmial.com>
2016-04-06 12:24:48 +0100

    fizzbuzz method ok

--- lib/my_fizz_buzz.rb
+++ lib/my_fizz_buzz.rb
@@ -7,6 +7,21 @@ class MyFizzBuzz
 
   end
 
+  def get_range
+    begin_at = (@page - 1) * @per_page
+    stop_at = @page * @per_page
+    (begin_at..stop_at).to_a.each do |val|
+
+    end
+  end
+
+  def fizz_buzz(val)
+    ret = ''
+    ret += 'Fizz' if val % 3 == 0
+    ret += 'Buzz' if val % 5 == 0
+    ret.empty? ? val : ret
+  end
+
   def defaults
     {
         page:     1,
--- spec/lib/my_fizz_buzz_spec.rb
+++ spec/lib/my_fizz_buzz_spec.rb
@@ -13,13 +13,29 @@ require Rails.root.join('lib/my_fizz_buzz')
 
 RSpec.describe "MyFizzBuzz" do
 
-  it "returns 1" do
+  before do
     args = {
         page: 2,
         per_page: 10
     }
-    my_fizz_buzz = MyFizzBuzz.new(args)
-    expect(my_fizz_buzz.get_values).to be == 1
+    @my_fizz_buzz = MyFizzBuzz.new(args)
+  end
+
+
+  it "returns Fizz if value % 3 == 0" do
+    expect(@my_fizz_buzz.fizz_buzz(9)).to be == 'Fizz'
+  end
+
+  it "returns Buzz if value % 5 == 0" do
+    expect(@my_fizz_buzz.fizz_buzz(25)).to be == 'Buzz'
+  end
+
+  it "returns FizzBuzz if value % 5 == 0 and value % 3 == 0" do
+    expect(@my_fizz_buzz.fizz_buzz(15)).to be == 'FizzBuzz'
+  end
+
+  it "returns value if value % 5 != 0 and value % 3 != 0" do
+    expect(@my_fizz_buzz.fizz_buzz(13)).to be == 13
   end
 
 end
