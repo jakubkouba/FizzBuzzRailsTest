41a4a50bd68a3bc500070ec67e3aa5ed3d68b92b
Jakub Adler <adler.jakub@gmial.com>
2016-04-06 11:58:39 +0100

    MyFizzBuzz class setup

---
+++ spec/lib/my_fizz_buzz_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# RSpec.configure do |config|
+#   config.infer_spec_type_from_file_location!
+# end
+
+# I Couldn't make Rspec autoload MyFizzBuzz class
+# file locations seems to be ok, even given implicit configuration
+# I don't know what I oversee
+# I can't require it even by relative way so I had to use abs path
+
+require Rails.root.join('lib/my_fizz_buzz')
+
+RSpec.describe "MyFizzBuzz" do
+
+  it "returns 1" do
+    my_fizz_buzz = MyFizzBuzz.new(page: 2, per_page: 10)
+    expect(my_fizz_buzz.get_values).to be == 1
+  end
+
+end
