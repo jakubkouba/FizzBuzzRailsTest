ceddf388c184a2ff6a29b901ed25c4873d73c5d0
Jakub Adler <adler.jakub@gmial.com>
2016-04-06 16:52:32 +0100

    pagination done

--- app/controllers/fizz_buzz_controller.rb
+++ app/controllers/fizz_buzz_controller.rb
@@ -2,13 +2,16 @@ class FizzBuzzController < ApplicationController
 
   def index
     @my_fizz_buzz = MyFizzBuzz.new(listing_params)
-    @values = @my_fizz_buzz.fizz_buzz_page
+    @values       = @my_fizz_buzz.fizz_buzz_page
+    @page         = listing_params[:page]
+    @per_page     = listing_params[:per_page]
+    @max_items    = 100_000
   end
 
   def listing_params
     {
-        page:     params[:page].nil? ? 1 : params[:page] ,
-        per_page: params[:per_page].nil? ? 100 : params[:per_page]
+        page:     params[:page].nil? ? 1 : params[:page].to_i ,
+        per_page: params[:per_page].nil? ? 100 : params[:per_page].to_i
     }
   end
 
