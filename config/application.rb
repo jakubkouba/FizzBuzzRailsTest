9c0d034785a3d1adb15604fd0925f1d5456e2e50
Jakub Adler <adler.jakub@gmial.com>
2016-04-06 14:41:08 +0100

    MyFizzBuzz added to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module FizzBuzz
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ## include libs
+    config.autoload_paths += ['lib']
   end
 end
