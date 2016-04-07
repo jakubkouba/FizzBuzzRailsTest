cfa0303917db416fe563720089afca162721ed38
Jakub Adler <adler.jakub@gmial.com>
2016-04-07 19:35:44 +0100

    pagination locals dried up

--- app/controllers/fizz_buzz_controller.rb
+++ app/controllers/fizz_buzz_controller.rb
@@ -1,18 +1,31 @@
 class FizzBuzzController < ApplicationController
 
   def index
-    @my_fizz_buzz = MyFizzBuzz.new(listing_params)
-    @values       = @my_fizz_buzz.fizz_buzz_page
-    @page         = listing_params[:page]
-    @per_page     = listing_params[:per_page]
-    @max_items    = 100_000
+    @facade = fizz_buzz_index_facade.to_h
+    @facade[:pages] = fizz_buzz_index_facade.pages
   end
 
   def listing_params
     {
-        page:     params[:page].nil? ? 1 : params[:page].to_i ,
-        per_page: params[:per_page].nil? ? 100 : params[:per_page].to_i
+        page:     params[:page].nil? ? DEF_PAGE_NUM : params[:page].to_i ,
+        per_page: params[:per_page].nil? ? DEF_PER_PAGE_NUM : params[:per_page].to_i
     }
+  end
+
+  def fizz_buzz_index_facade
+    index_view = Struct.new(:values, :page, :per_page, :max_items) do
+
+      def pages
+        (self.max_items / self.per_page).to_i
+      end
+
+    end
+
+    index_view.new(fizz_buzz_values, *listing_params.values, MAX_ITEMS)
+  end
+
+  def fizz_buzz_values
+     MyFizzBuzz.new(listing_params).fizz_buzz_page
   end
 
 end
--- spec/controllers/fizz_buzz_controller_spec.rb
+++ spec/controllers/fizz_buzz_controller_spec.rb
@@ -2,17 +2,29 @@ require 'rails_helper'
 
 RSpec.describe FizzBuzzController, type: :controller do
 
+  before { get :index }
+
   describe "GET #index" do
     it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
     end
 
-    it "assings @values" do
-      get :index
-
-      expect(assigns(:values)).to be_an_instance_of Array
+    it "assings view values" do
+      expect(assigns(:facade)).to be_an Hash
     end
+
+    describe "fizz_buzz_index_facade" do
+
+      subject(:facade_obj) { controller.fizz_buzz_index_facade }
+
+      it { is_expected.to respond_to(:page) }
+
+      it "returns int when call page" do
+        expect(facade_obj.pages).to be_an Integer
+      end
+
+    end
+
   end
 
 end
