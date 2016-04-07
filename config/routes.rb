3fdff7152b8bfcbb49646a85552a33cd8eedf310
Jakub Adler <adler.jakub@gmial.com>
2016-04-07 20:46:12 +0100

    post route set for pagination submit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'fizz_buzz#index', as: :home
 
+  match '/show', to: 'fizz_buzz#index', as: :show, via: :post
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
